c1fcfe58ddf231a5ab93510d651b7fbfdb7094f4
eruuc <kim.eric3@northeastern.edu>
2025-09-19 16:37:39 -0400

    attempt fixes for promise

--- app/(Kambaz)/Courses/1001/page.tsx
+++ app/(Kambaz)/Courses/1001/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 
 
-export default async function CoursesPage({ params, }: { params: Promise<{ cid: string }>; }) {
- const { cid } = await params;
- redirect(`/Courses/1001/Home`);
+export default async function CoursesPage({ params, }: { params: { cid: string }; }) {
+ const { cid } = params;
+ redirect(`/Courses/${cid}/Home`);
 }
