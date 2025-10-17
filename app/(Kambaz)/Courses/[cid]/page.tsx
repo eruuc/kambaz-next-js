d7f1f91b2fcf988655570274cd01099241035866
eruuc <kim.eric3@northeastern.edu>
2025-10-17 15:50:52 -0400

    Fix course id redirection

--- app/(Kambaz)/Courses/[cid]/page.tsx
+++ app/(Kambaz)/Courses/[cid]/page.tsx
@@ -3,5 +3,5 @@ import { redirect } from "next/navigation";
 
 export default function CoursesPage({ params }: { params: { cid: string }; }) {
  const { cid } = params;
- redirect(`/Courses/1001/Home`);
+ redirect(`/Courses/${cid}/Home`);
 }
