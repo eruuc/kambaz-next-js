92e4a9bc5fa552d960673bcde6427c3c210335c7
eruuc <kim.eric3@northeastern.edu>
2025-10-17 15:59:33 -0400

    Fix redirection

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -2,38 +2,36 @@ import { ReactNode } from "react";
 import { FaAlignJustify } from "react-icons/fa";
 import CourseNavigation from "./Navigation";
 import { courses } from "../../Database";
-export async function CoursesLayout({ children, params }: Readonly<{ children: ReactNode; params: { cid: string }; }>) {
+
+interface CoursesLayoutProps {
+  children: ReactNode;
+  params: { cid: string };
+}
+
+export default async function CoursesLayout(props: Readonly<CoursesLayoutProps>) {
+  const { children, params } = props;
   const { cid } = params;
+
   const course = courses.find((course) => course._id === cid);
+
   return (
     <div id="wd-courses">
-    <h2 className="text-danger">
+      <h2 className="text-danger">
         <FaAlignJustify className="me-4 fs-4 mb-1" />
-        {course?.name} </h2> <hr />
-          <h2 className="text-danger">
-      <FaAlignJustify className="me-4 fs-4 mb-1" />
-      Course {cid} </h2> <hr />
-    <div className="d-flex">
-      <div className="d-none d-md-block">
-        <CourseNavigation />
+        {course?.name}
+      </h2>
+      <hr />
+      <h2 className="text-danger">
+        <FaAlignJustify className="me-4 fs-4 mb-1" />
+        Course {cid}
+      </h2>
+      <hr />
+      <div className="d-flex">
+        <div className="d-none d-md-block">
+          <CourseNavigation />
+        </div>
+        <div className="flex-fill">{children}</div>
       </div>
-      <div className="flex-fill">
-        {children}
-      </div></div>
-  </div>
-);}
-
-//  return (
-//    <div id="wd-courses">
-//      <h2>Courses {cid}</h2>
-//      <hr />
-//      <table>
-//        <tbody>
-//          <tr>
-//            <td valign="top" width="200"> <CourseNavigation /> </td>
-//            <td valign="top" width="100%"> {children} </td>
-//          </tr>
-//        </tbody>
-//      </table>
-//    </div>
-// );}
+    </div>
+  );
+}
