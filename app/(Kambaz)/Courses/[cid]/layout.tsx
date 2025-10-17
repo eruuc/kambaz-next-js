657fedb031ec9f951f9985ad0bbe0f5b66f09039
eruuc <kim.eric3@northeastern.edu>
2025-10-17 16:02:04 -0400

    Fix redirection

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -1,37 +1,35 @@
 import { ReactNode } from "react";
 import { FaAlignJustify } from "react-icons/fa";
 import CourseNavigation from "./Navigation";
-import { courses } from "../../Database";
-
-interface CoursesLayoutProps {
-  children: ReactNode;
-  params: { cid: string };
-}
-
-export default async function CoursesLayout(props: Readonly<CoursesLayoutProps>) {
-  const { children, params } = props;
-  const { cid } = params;
-
-  const course = courses.find((course) => course._id === cid);
-
+export default async function CoursesLayout(){
+  //   { children, params }: Readonly<{ children: ReactNode; params: { cid: string }; }>) {
+//  const { cid } = params;
   return (
     <div id="wd-courses">
-      <h2 className="text-danger">
+    <h2 className="text-danger">
         <FaAlignJustify className="me-4 fs-4 mb-1" />
-        {course?.name}
-      </h2>
-      <hr />
-      <h2 className="text-danger">
-        <FaAlignJustify className="me-4 fs-4 mb-1" />
-        Course {cid}
-      </h2>
-      <hr />
-      <div className="d-flex">
-        <div className="d-none d-md-block">
-          <CourseNavigation />
-        </div>
-        <div className="flex-fill">{children}</div>
+        Course 1001 </h2> <hr />
+    <div className="d-flex">
+      <div className="d-none d-md-block">
+        <CourseNavigation />
       </div>
-    </div>
-  );
-}
+      <div className="flex-fill">
+        
+      </div></div>
+  </div>
+);}
+
+//  return (
+//    <div id="wd-courses">
+//      <h2>Courses {cid}</h2>
+//      <hr />
+//      <table>
+//        <tbody>
+//          <tr>
+//            <td valign="top" width="200"> <CourseNavigation /> </td>
+//            <td valign="top" width="100%"> {children} </td>
+//          </tr>
+//        </tbody>
+//      </table>
+//    </div>
+// );}
